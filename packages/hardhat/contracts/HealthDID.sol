76028d96a344c5e72ec9f69782b59269c7f2897a
shyam-patel-kira <shyampkira@gmail.com>
2023-09-23 17:57:54 -0400

    completed chain agnostic registration function

--- packages/hardhat/contracts/HealthDID.sol
+++ packages/hardhat/contracts/HealthDID.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.17;
 
 import "./lib/Structs.sol";
 
-contract HealthDID {
+contract HealthDIDRegistry {
     // event didRegistered();
     // event didUpdated();
     // mapping(string => mapping(address => Structs.HealthDID)) private didResolver;
-    mapping(string => address) private didOwnerAddress;
+    mapping(string => address) private didOwnerAddressRegistry;
     mapping(address => Structs.HealthDID) public addressDidMapping;
 
     modifier onlyOwner(address owner) {
@@ -18,40 +18,55 @@ contract HealthDID {
     constructor() {}
 
     function getHealtDID(string memory _healthDid) public view returns (Structs.HealthDID memory) {
-        return addressDidMapping[didOwnerAddress[_healthDid]];
+        return addressDidMapping[didOwnerAddressRegistry[_healthDid]];
     }
 
     function registerDID(string memory _healthDID, string memory _uri) public returns (bool) {
-        require(didOwnerAddress[_healthDID] != address(0), "DID already exists");
+        require(didOwnerAddressRegistry[_healthDID] == address(0), "DID already exists");
+        require(resolveChainId(_healthDID) == getChainID(), "Incorrect Chain Id in DID");
+
+        didOwnerAddressRegistry[_healthDID] = msg.sender;
+        addressDidMapping[msg.sender].owner = msg.sender;
+        addressDidMapping[msg.sender].healthDid = _healthDID;
+        addressDidMapping[msg.sender].ipfsUri = _uri;
+        addressDidMapping[msg.sender].hasWorldId = false;
+        addressDidMapping[msg.sender].hasPolygonId = false;
+        addressDidMapping[msg.sender].hasSocialId = false;
+        addressDidMapping[msg.sender].reputationScore = 10;
+
+        return true;
     }
 
-    function extractChainIdAndReturnUint(string memory did) public pure returns (uint) {
-        require(bytes(did).length == 44, "Invalid DID string length");
+    function resolveChainId(string memory did) public pure returns (uint256) {
+        require(bytes(did).length >= 6, "Input string too short");
 
-        // Convert the string to bytes
         bytes memory didBytes = bytes(did);
+        uint256 chainIdUint = 0;
 
-        // Extract the first 6 characters as the chainId
-        bytes6 chainId;
-        assembly {
-            chainId := mload(add(didBytes, 32))
+        for (uint256 i = 0; i < 6; i++) {
+            uint8 charValue = uint8(didBytes[i]);
+            require(charValue >= 48 && charValue <= 57, "Not a valid number"); // ASCII values for 0-9
+            chainIdUint = chainIdUint * 10 + (charValue - 48); // converting ASCII to integer and accumulating
         }
-
-        // Convert the chainId to a uint
-        uint chainIdUint = uint(chainId);
-
         return chainIdUint;
     }
 
+    function getChainID() public view returns (uint256) {
+        uint256 id;
+        assembly {
+            id := chainid()
+        }
+        return id;
+    }
 
-    function stringToBytes32(string memory source) internal pure returns (bytes32 result) {
-        bytes memory tempEmptyStringTest = bytes(source);
+    function stringToBytes32(string memory _source) public pure returns (bytes32 _result) {
+        bytes memory tempEmptyStringTest = bytes(_source);
         if (tempEmptyStringTest.length == 0) {
             return 0x0;
         }
 
         assembly {
-            result := mload(add(source, 32))
+            _result := mload(add(_source, 32))
         }
     }
 }
