11201e700e0ada42afbf6ef05d0bb89220d909d5
metacare <metacare@dcl.health>
2023-10-05 17:06:16 -0400

    updated patient type

--- packages/nextjs/types/abitype/fhir.ts
+++ packages/nextjs/types/abitype/fhir.ts
@@ -1,32 +1,126 @@
 export type FHIRPatient = {
-  id: string;
-  did: string;
   resourceType: "Patient";
-  identifier?: {
-    use?: "usual" | "official" | "temp" | "secondary" | string;
+  id?: string;
+  identifier?: Array<{
     system?: string;
-    value: string;
-  }[];
-  name?: {
-    family: string;
-    given: string;
-  }[];
-  telecom?: {
-    system: "phone" | "email" | string;
-    value: string;
-    use?: "home" | "work" | "temp" | "old" | string;
-  }[];
-  address?: {
-    line?: string[];
+    value?: string;
+  }>;
+  active?: boolean;
+  name?: Array<{
+    use?: string;
+    family?: string;
+    given?: Array<string>;
+    prefix?: Array<string>;
+    suffix?: Array<string>;
+  }>;
+  telecom?: Array<{
+    system?: string;
+    value?: string;
+    use?: string;
+    rank?: number;
+  }>;
+  gender?: string;
+  birthDate?: string;
+  deceasedBoolean?: boolean;
+  address?: Array<{
+    use?: string;
+    type?: string;
+    text?: string;
+    line?: Array<string>;
     city?: string;
+    district?: string;
     state?: string;
     postalCode?: string;
     country?: string;
-  }[];
-  birthDate?: string;
-  gender?: "male" | "female" | "other" | "unknown";
-  extension?: {
-    url: string;
-    valueString?: string;
-  }[]; // Used to capture extra fields like birthsex and ethnicity
+    period?: {
+      start?: string;
+      end?: string;
+    };
+  }>;
+  maritalStatus?: {
+    coding?: Array<{
+      system?: string;
+      code?: string;
+      display?: string;
+    }>;
+    text?: string;
+  };
+  multipleBirthBoolean?: boolean;
+  photo?: Array<{
+    contentType?: string;
+    data?: string;
+  }>;
+  contact?: Array<{
+    relationship?: Array<{
+      coding?: Array<{
+        system?: string;
+        code?: string;
+        display?: string;
+      }>;
+      text?: string;
+    }>;
+    name?: {
+      use?: string;
+      family?: string;
+      given?: Array<string>;
+      prefix?: Array<string>;
+      suffix?: Array<string>;
+    };
+    telecom?: Array<{
+      system?: string;
+      value?: string;
+      use?: string;
+      rank?: number;
+    }>;
+    address?: {
+      use?: string;
+      type?: string;
+      text?: string;
+      line?: Array<string>;
+      city?: string;
+      district?: string;
+      state?: string;
+      postalCode?: string;
+      country?: string;
+      period?: {
+        start?: string;
+        end?: string;
+      };
+    };
+    gender?: string;
+    organization?: {
+      reference?: string;
+      display?: string;
+    };
+    period?: {
+      start?: string;
+      end?: string;
+    };
+  }>;
+  communication?: Array<{
+    language?: {
+      coding?: Array<{
+        system?: string;
+        code?: string;
+        display?: string;
+      }>;
+      text?: string;
+    };
+    preferred?: boolean;
+  }>;
+  generalPractitioner?: Array<{
+    reference?: string;
+    display?: string;
+  }>;
+  managingOrganization?: {
+    reference?: string;
+    display?: string;
+  };
+  link?: Array<{
+    other?: {
+      reference?: string;
+      display?: string;
+    };
+    type?: string;
+  }>;
 };
